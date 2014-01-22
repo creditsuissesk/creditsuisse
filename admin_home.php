f63d04ca0e0bd4e5dc01c9d58c0cd2477b30d8b9
KUNAL@KunalShah <kunshin93@yahoo.com>
2014-01-22 09:56:01 +0530

    updated with table header bug

--- admin_home.php
+++ admin_home.php
@@ -192,20 +192,23 @@ function clearText(field)
     else if (field.value == '') field.value = field.defaultValue;
 }
 </script>
-
+<!---
+script for js/list.js tables
+--->
+<script src="js/list.js"></script>
 <script type="text/javascript" src="js/jquery.min.js"></script> 
 <script type="text/javascript" src="js/jquery.scrollTo-min.js"></script> 
 <script type="text/javascript" src="js/jquery.localscroll-min.js"></script> 
 <script type="text/javascript" src="js/init.js"></script>  
 <link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" />
 <style type="text/css">
-body,td,th {
+body,td {
 	color: #000000;
 	font-size: 14px;
 }
 </style>
 <link href="SpryAssets/SpryRating.css" rel="stylesheet" type="text/css">
-<link href="css/table.css" rel="stylesheet" type="text/css" />
+<link href="css/table.css?12" rel="stylesheet" type="text/css" />
 <script src="SpryAssets/SpryRating.js" type="text/javascript"></script>
  
 
@@ -229,28 +232,34 @@ else {
   <div class="TabbedPanelsContentGroup">
     <div class="TabbedPanelsContent">
     <!--- if no new users then skip whole table--->
-    <?php if ($totalRows_update>0 ) { ?>
-        <div id="curr_courses">
+    
+        <div id="new_users">
     <div class="datagrid">
+    <?php if ($totalRows_update>0 ) { ?>
     <table>
     <thead>
+    
   <tr>
-    <td>User Id</td>
-    <td>User Name</td>
-    <td>Password</td>
-    <td>First Name</td>
-    <td>Last Name</td>
-    <td>Contact No</td>
-    <td>Date of Birth</td>
-    <td>Institute</td>
-    <td>Stream</td>
-    <td>Role</td>
-    <td>Final Status</td>
-    <td>    </td>
+  	 <th class="sort" data-sort="User_Id">User Id</th>
+    <th class="sort" data-sort="User_Name">User Name</th>
+     <th class="sort" data-sort="Password">Password</th>
+    <th class="sort" data-sort="First_Name">First Name</th>
+    <th class="sort" data-sort="Last_Name">Last Name</th>
+    <th class="sort" data-sort="Contact_No.">Contact No</th>
+    <th class="sort" data-sort="date_of_birth">Date of Birth</th>
+    <th class="sort" data-sort="Institute">Institute</th>
+    <th class="sort" data-sort="stream">Stream</th>
+    <th class="sort" data-sort="role">Role</th>
+    <th>Final Status</th>
+    <th>    </th>
+    <th colspan="2">
+          <input type="text" class="search" placeholder="Search New User" />
+        </th>
   </tr>
   </thead>
     <a href="<?php echo $logoutAction ?>">
   </a>
+  <tbody class="list">
   <?php do { ?>
     
   
@@ -278,11 +287,20 @@ else {
         </td></form>
     </tr>
     <?php } while ($row_update = mysql_fetch_assoc($update)); ?>
+    </tbody>
     </table>
     </div></div>
     <?php } else {
 		echo "No new users";
 	} ?>
+    <script>
+var currOptions = {
+  valueNames: [ 'User_Id','User_Name','Password','First_Name','Last_Name','Contact_No.','date_of_birth','Institute','stream','role']
+};
+
+// Init list
+var currList = new List('new_users', currOptions);
+</script>
     </div>
     <!--start of existing users' tab content--><div class="TabbedPanelsContent">
     <?php if ($totalRows_all_users>0 ) { ?>
@@ -291,29 +309,32 @@ else {
     <table>
     <thead>
   <tr>
-    <td>User Id</td>
-    <td>User Name</td>
-    <td>Password</td>
-    <td>First Name</td>
-    <td>Last Name</td>
-    <td>Contact No</td>
-    <td>Date of Birth</td>
-    <td>Institute</td>
-    <td>Stream</td>
-    <td>Role</td>
-    <td>Current Status</td>
-    <td>    </td>
+  
+  	 <th class="sort" data-sort="currentid">User Id</th>
+    <th class="sort" data-sort="name">User Name</th>
+    <th class="sort" data-sort="cuname">First Name</th>
+    <th class="sort" data-sort="curname">Last Name</th>
+    <th class="sort" data-sort="cno">Contact No</th>
+    <th class="sort" data-sort="cdate">Date of Birth</th>
+    <th class="sort" data-sort="cinstitute">Institute</th>
+    <th class="sort" data-sort="cstream">Stream</th>
+    <th class="sort" data-sort="crole">Role</th>
+    <th class="sort" data-sort="cstatus">Current Status</th>
+    <th>    </th>
+    <th colspan="2">
+          <input type="text" class="search" placeholder="Search Existing User" />
+        </th>
   </tr>
   </thead>
   <a href="<?php echo $logoutAction ?>">
   </a>
+  <tbody class="list">
   <?php do { ?>
     
   
     <tr>
       <td><?php echo $row_all_users['u_id']; ?></td>
       <td><?php echo $row_all_users['u_name']; ?></td>
-      <td><?php echo $row_all_users['password']; ?></td>
       <td><?php echo $row_all_users['f_name']; ?></td>
       <td><?php echo $row_all_users['l_name']; ?></td>
       <td><?php echo $row_all_users['contact_no']; ?></td>
@@ -331,12 +352,20 @@ else {
         </td></form>
     </tr>
     <?php } while ($row_all_users = mysql_fetch_assoc($all_users)); ?>
+    </tbody>
     </table>
     </div></div>
     <?php } else {
 		echo "No Existing users";
 	} ?>
-    
+    <script>
+var cOptions = {
+  valueNames: [ 'currentid','name','cuname','curname','cno','cdate','cinstitute','cstream','crole','cstatus']
+};
+
+// Init list
+var cuList = new List('existing_users', cOptions);
+</script>
     </div><!--end of existing users' tab content-->
   </div>
 </div>
