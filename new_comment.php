d38be613011633fbbf8f2fc7a68be67b672a62e9
Shaarad Dalvi <dalvishaarad@gmail.com>
2014-01-30 16:53:27 +0530

    user can comment (with a bug)

---
+++ new_comment.php
@@ -0,0 +1,98 @@
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+$MM_authorizedUsers = "admin,student,author,cm";
+$MM_donotCheckaccess = "false";
+
+// *** Restrict Access To Page: Grant or deny access to this page
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+  // For security, start by assuming the visitor is NOT authorized. 
+  $isValid = False; 
+
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+  if (!empty($UserName)) { 
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
+    // Parse the strings into arrays. 
+    $arrUsers = Explode(",", $strUsers); 
+    $arrGroups = Explode(",", $strGroups); 
+    if (in_array($UserName, $arrUsers)) { 
+      $isValid = true; 
+    } 
+    // Or, you may restrict access to only certain users based on their username. 
+    if (in_array($UserGroup, $arrGroups)) { 
+      $isValid = true; 
+    } 
+    if (($strUsers == "") && false) { 
+      $isValid = true; 
+    } 
+  } 
+  return $isValid; 
+}
+
+$MM_restrictGoTo = "index.php";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+  $MM_qsChar = "?";
+  $MM_referrer = $_SERVER['PHP_SELF'];
+  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: ". $MM_restrictGoTo); 
+  exit;
+}
+?>
+<?php require_once('Connections/conn.php'); ?>
+<?php
+	if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+mysql_select_db($database_conn, $conn);
+if(isset($_POST['comment_body']) && isset($_POST['disc_id'])) {
+		echo "all POST set";
+		$create_comment = sprintf("INSERT INTO `comment` (discussion_id, insert_uid, comment_body) VALUES (%s, %s, %s)",
+							 GetSQLValueString($_POST['disc_id'], "int"),
+							 GetSQLValueString($_SESSION['MM_UserID'], "int"),
+							 GetSQLValueString($_POST['comment_body'], "text"));
+	  
+		$Result1 = mysql_query($create_comment, $conn) or die(mysql_error());
+		
+		$update_comment_count =sprintf("UPDATE `discussion` SET count_comments=count_comments+1 WHERE discussion_id=%s",GetSQLValueString($_POST['disc_id'], "int"));
+		$Result2 = mysql_query($update_comment_count, $conn) or die(mysql_error());
+	  
+		$insertGoTo = "index.php";
+		if (isset($_SERVER['QUERY_STRING'])) {
+		  $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+		  $insertGoTo .= $_SERVER['QUERY_STRING'];		 
+		}
+	}
+?>
