80c983b1c8ca40da5c61b87efeee797dab23a60f
Shaarad Dalvi <dalvishaarad@gmail.com>
2014-02-09 17:44:15 +0530

    changed foreign key actions to cascade

--- credit_suisse.sql
+++ credit_suisse.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2014 at 01:07 PM
+-- Generation Time: Feb 09, 2014 at 01:13 PM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -67,8 +67,6 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `discussion_id` int(11) NOT NULL,
   `insert_uid` int(11) unsigned NOT NULL,
   `date_inserted_c` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `delete_uid` int(11) unsigned DEFAULT NULL,
-  `date_deleted_c` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `update_uid` int(11) unsigned DEFAULT NULL,
   `date_updated_c` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `comment_body` text NOT NULL,
@@ -76,24 +74,24 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `comment_score` float NOT NULL DEFAULT '0',
   PRIMARY KEY (`comment_id`),
   KEY `insert_uid` (`insert_uid`),
-  KEY `delete_uid` (`delete_uid`),
   KEY `update_uid` (`update_uid`),
   KEY `discussion_id` (`discussion_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `comment`
 --
 
-INSERT INTO `comment` (`comment_id`, `discussion_id`, `insert_uid`, `date_inserted_c`, `delete_uid`, `date_deleted_c`, `update_uid`, `date_updated_c`, `comment_body`, `flag`, `comment_score`) VALUES
-(2, 2, 5, '2014-01-19 06:40:07', NULL, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', 'This is comment body!', 0, 1),
-(3, 2, 3, '2014-01-19 22:30:31', NULL, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', 'new Comment!', 0, 1),
-(5, 4, 5, '2014-01-30 11:31:44', NULL, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', ' lol!', 0, 0),
-(6, 2, 5, '2014-01-30 11:46:53', NULL, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', ' this is posted from forums!', 0, 0),
-(7, 8, 5, '2014-01-30 12:02:25', NULL, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', ' temp comment to test redirection!', 0, 0),
-(8, 2, 12, '2014-01-30 12:51:49', NULL, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', ' comment by photouser!', 0, -1),
-(9, 10, 12, '2014-01-31 08:55:16', NULL, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', ' hmmm...looks like it is being updated. now testing if the comment count is being updated or not...', 0, 0),
-(10, 2, 1, '2014-02-08 05:04:57', NULL, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', ' this is worst discussion i had', 0, 0);
+INSERT INTO `comment` (`comment_id`, `discussion_id`, `insert_uid`, `date_inserted_c`, `update_uid`, `date_updated_c`, `comment_body`, `flag`, `comment_score`) VALUES
+(2, 2, 5, '2014-01-19 06:40:07', NULL, '0000-00-00 00:00:00', 'This is comment body!', 0, 1),
+(3, 2, 3, '2014-01-19 22:30:31', NULL, '0000-00-00 00:00:00', 'new Comment!', 0, 1),
+(5, 4, 5, '2014-01-30 11:31:44', NULL, '0000-00-00 00:00:00', ' lol!', 0, 0),
+(6, 2, 5, '2014-01-30 11:46:53', NULL, '0000-00-00 00:00:00', ' this is posted from forums!', 0, 0),
+(7, 8, 5, '2014-01-30 12:02:25', NULL, '0000-00-00 00:00:00', ' temp comment to test redirection!', 0, 0),
+(8, 2, 12, '2014-01-30 12:51:49', NULL, '0000-00-00 00:00:00', ' comment by photouser!', 0, -1),
+(9, 10, 12, '2014-01-31 08:55:16', NULL, '0000-00-00 00:00:00', ' hmmm...looks like it is being updated. now testing if the comment count is being updated or not...', 0, 0),
+(10, 2, 1, '2014-02-08 05:04:57', NULL, '0000-00-00 00:00:00', ' this is worst discussion i had', 0, 0),
+(11, 4, 1, '2014-02-09 11:31:29', NULL, '0000-00-00 00:00:00', ' this is new comment to test bookmark comment notification', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -172,6 +170,7 @@ CREATE TABLE IF NOT EXISTS `discussion` (
   `count_comments` int(11) NOT NULL DEFAULT '0',
   `date_inserted_d` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_updated_d` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `flag` tinyint(4) NOT NULL DEFAULT '0',
   `rating` float NOT NULL DEFAULT '0',
   PRIMARY KEY (`discussion_id`),
   KEY `category_id` (`category_id`),
@@ -183,13 +182,13 @@ CREATE TABLE IF NOT EXISTS `discussion` (
 -- Dumping data for table `discussion`
 --
 
-INSERT INTO `discussion` (`discussion_id`, `type`, `category_id`, `insert_uid`, `last_comment_id`, `name`, `disc_body`, `count_comments`, `date_inserted_d`, `date_updated_d`, `rating`) VALUES
-(2, 0, 1, 5, 0, 'First discussion', 'First discussion body!', 3, '2014-01-19 04:13:19', '0000-00-00 00:00:00', 6),
-(3, 0, 1, 8, 0, 'This is by Kunal', 'Kunal''s body! :P', 0, '2014-01-19 04:38:46', '0000-00-00 00:00:00', 1),
-(4, 0, 2, 5, 0, 'Is this forum style okay?', 'We''ll continue this is it looks good..', 1, '2014-01-19 04:42:45', '0000-00-00 00:00:00', -1),
-(8, 0, 1, 5, 0, 'new disc title', 'new disc body ', 1, '2014-01-30 09:12:29', '0000-00-00 00:00:00', 0),
-(9, 0, 1, 5, 0, 'testing new discussion', 'discussion body ', 0, '2014-01-30 11:58:56', '0000-00-00 00:00:00', 0),
-(10, 0, 1, 12, 0, 'testing update count', ' This discussion is being opened to test if the user''s discussion count is being updated or not.', 1, '2014-01-31 08:54:19', '0000-00-00 00:00:00', 0);
+INSERT INTO `discussion` (`discussion_id`, `type`, `category_id`, `insert_uid`, `last_comment_id`, `name`, `disc_body`, `count_comments`, `date_inserted_d`, `date_updated_d`, `flag`, `rating`) VALUES
+(2, 0, 1, 5, 0, 'First discussion', 'First discussion body!', 3, '2014-01-19 04:13:19', '0000-00-00 00:00:00', 0, 6),
+(3, 0, 1, 8, 0, 'This is by Kunal', 'Kunal''s body! :P', 0, '2014-01-19 04:38:46', '0000-00-00 00:00:00', 0, 1),
+(4, 0, 2, 5, 0, 'Is this forum style okay?', 'We''ll continue this is it looks good..', 2, '2014-01-19 04:42:45', '2014-02-09 11:31:29', 0, -1),
+(8, 0, 1, 5, 0, 'new disc title', 'new disc body ', 1, '2014-01-30 09:12:29', '0000-00-00 00:00:00', 0, 0),
+(9, 0, 1, 5, 0, 'testing new discussion', 'discussion body ', 0, '2014-01-30 11:58:56', '0000-00-00 00:00:00', 0, 0),
+(10, 0, 1, 12, 0, 'testing update count', ' This discussion is being opened to test if the user''s discussion count is being updated or not.', 1, '2014-01-31 08:54:19', '0000-00-00 00:00:00', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -340,7 +339,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`u_id`, `u_name`, `password`, `f_name`, `l_name`, `contact_no`, `dob`, `institute`, `stream`, `role`, `approve_id`, `photo`, `about`, `show_email`, `gender`, `user_score`, `count_bookmarks`, `created_comments`, `count_discussions`) VALUES
-(1, 'xyz@gmail.com', 'qwerty', 'Abdul', 'Shaikh', 2147483647, '1993-03-01', 'SPIT', 'Computers', 'student', 1, 'images/profiles/01.jpg', '', 0, 0, 0, 0, 1, 0),
+(1, 'xyz@gmail.com', 'qwerty', 'Abdul', 'Shaikh', 2147483647, '1993-03-01', 'SPIT', 'Computers', 'student', 1, 'images/profiles/01.jpg', '', 0, 0, 0, 0, 2, 0),
 (2, 'abc@tech.org', 'qwerty', 'Abhishek', 'Chaturvedi', 26845172, '1979-04-02', 'VJTI', 'Information Technolo', 'author', 1, 'images/profiles/02.jpg', '', 0, 0, 0, 0, 0, 0),
 (3, 'dalvishaarad@gmail.c', 'password', 'Shaarad', 'Dalvi', 2147483647, '1992-04-01', 'vjti', 'comps', 'student', 1, 'images/profiles/03.jpg', '', 0, 0, 2, 0, 0, 0),
 (4, 'shaaraddalvi@outlook.com', 'password', 'Shaarad', 'Inamdar', 25406266, '1993-11-01', 'TSEC', 'Electronics', 'student', 1, 'images/profiles/04.jpg', '', 0, 0, 0, 0, 0, 0),
@@ -375,15 +374,20 @@ CREATE TABLE IF NOT EXISTS `user_comment` (
 INSERT INTO `user_comment` (`user_comment_id`, `user_id`, `vote_status`, `bookmarked`, `date_last_viewed`) VALUES
 (2, 1, -1, 0, '2014-02-08 05:04:58'),
 (3, 1, 1, 0, '2014-02-08 05:04:58'),
+(5, 1, 0, 0, '2014-02-09 11:31:29'),
 (6, 1, 0, 0, '2014-02-08 05:04:58'),
 (8, 1, -1, 0, '2014-02-08 05:04:58'),
 (10, 1, 0, 0, '2014-02-08 05:04:58'),
-(2, 5, 0, 0, '2014-01-31 10:58:41'),
-(3, 5, 0, 0, '2014-01-31 10:58:41'),
-(5, 5, 0, 0, '2014-01-30 11:47:38'),
-(6, 5, 0, 0, '2014-01-31 10:58:41'),
+(11, 1, 0, 0, '2014-02-09 11:31:29'),
+(2, 5, 0, 0, '2014-02-09 10:08:14'),
+(3, 5, 0, 0, '2014-02-09 10:08:14'),
+(5, 5, 0, 0, '2014-02-09 11:32:11'),
+(6, 5, 0, 0, '2014-02-09 10:08:14'),
 (7, 5, 0, 0, '2014-01-30 12:02:25'),
-(8, 5, 0, 0, '2014-01-31 10:58:41'),
+(8, 5, 0, 0, '2014-02-09 10:08:14'),
+(9, 5, 0, 0, '2014-02-09 11:17:44'),
+(10, 5, 0, 0, '2014-02-09 10:08:14'),
+(11, 5, 0, 0, '2014-02-09 11:32:11'),
 (2, 12, 0, 0, '2014-02-03 11:38:18'),
 (3, 12, -1, 0, '2014-02-03 11:38:18'),
 (5, 12, 0, 0, '2014-02-03 11:53:00'),
@@ -417,12 +421,13 @@ CREATE TABLE IF NOT EXISTS `user_discussion` (
 INSERT INTO `user_discussion` (`u_id`, `user_discussion_id`, `seen_comments`, `date_last_viewed`, `bookmarked`, `vote_status`) VALUES
 (1, 2, 5, '2014-02-08 05:05:24', 0, -1),
 (1, 3, 0, '2014-01-24 07:27:27', 0, 0),
-(1, 4, 0, '2014-01-24 07:27:39', 0, 0),
-(5, 2, 4, '2014-01-31 10:58:41', 0, 0),
-(5, 3, 0, '2014-01-30 11:37:26', 0, 0),
-(5, 4, 1, '2014-01-30 11:47:38', 0, 0),
-(5, 8, 1, '2014-01-30 12:02:25', 0, 0),
+(1, 4, 2, '2014-02-09 11:31:29', 0, 0),
+(5, 2, 5, '2014-02-09 11:16:41', 1, 0),
+(5, 3, 0, '2014-02-09 10:08:10', 0, 0),
+(5, 4, 2, '2014-02-09 11:32:10', 1, 0),
+(5, 8, 1, '2014-02-09 11:16:42', 1, 0),
 (5, 9, 0, '2014-01-30 11:59:18', 0, 0),
+(5, 10, 1, '2014-02-09 11:17:43', 1, 0),
 (12, 2, 4, '2014-02-03 11:38:18', 1, 0),
 (12, 4, 1, '2014-02-03 11:53:00', 0, 0),
 (12, 8, 1, '2014-02-03 09:56:01', 0, 0),
@@ -437,65 +442,65 @@ INSERT INTO `user_discussion` (`u_id`, `user_discussion_id`, `seen_comments`, `d
 -- Constraints for table `approved_status`
 --
 ALTER TABLE `approved_status`
-  ADD CONSTRAINT `approved_status_ibfk_1` FOREIGN KEY (`r_id`) REFERENCES `resource` (`r_id`),
-  ADD CONSTRAINT `approved_status_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
+  ADD CONSTRAINT `approved_status_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `approved_status_ibfk_1` FOREIGN KEY (`r_id`) REFERENCES `resource` (`r_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`insert_uid`) REFERENCES `user` (`u_id`),
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`delete_uid`) REFERENCES `user` (`u_id`),
-  ADD CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`update_uid`) REFERENCES `user` (`u_id`),
-  ADD CONSTRAINT `comment_ibfk_4` FOREIGN KEY (`discussion_id`) REFERENCES `discussion` (`discussion_id`);
+  ADD CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`update_uid`) REFERENCES `user` (`u_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`insert_uid`) REFERENCES `user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_4` FOREIGN KEY (`discussion_id`) REFERENCES `discussion` (`discussion_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `create_course`
 --
 ALTER TABLE `create_course`
-  ADD CONSTRAINT `c_id_key` FOREIGN KEY (`c_id`) REFERENCES `course` (`c_id`);
+  ADD CONSTRAINT `c_id_key` FOREIGN KEY (`c_id`) REFERENCES `course` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `create_course_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `discussion`
 --
 ALTER TABLE `discussion`
-  ADD CONSTRAINT `discussion_ibfk_3` FOREIGN KEY (`insert_uid`) REFERENCES `user` (`u_id`),
-  ADD CONSTRAINT `discussion_ibfk_4` FOREIGN KEY (`category_id`) REFERENCES `discussion_category` (`category_id`);
+  ADD CONSTRAINT `discussion_ibfk_3` FOREIGN KEY (`insert_uid`) REFERENCES `user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `discussion_ibfk_4` FOREIGN KEY (`category_id`) REFERENCES `discussion_category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `enroll_course`
 --
 ALTER TABLE `enroll_course`
-  ADD CONSTRAINT `enroll_course_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `course` (`c_id`),
-  ADD CONSTRAINT `enroll_course_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
+  ADD CONSTRAINT `enroll_course_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `course` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `enroll_course_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `rate_resource`
 --
 ALTER TABLE `rate_resource`
-  ADD CONSTRAINT `rate_resource_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`),
-  ADD CONSTRAINT `rate_resource_ibfk_3` FOREIGN KEY (`r_id`) REFERENCES `resource` (`r_id`);
+  ADD CONSTRAINT `rate_resource_ibfk_3` FOREIGN KEY (`r_id`) REFERENCES `resource` (`r_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rate_resource_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `resource`
 --
 ALTER TABLE `resource`
-  ADD CONSTRAINT `resource_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `course` (`c_id`),
-  ADD CONSTRAINT `resource_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `resource_type` (`type_id`);
+  ADD CONSTRAINT `resource_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `resource_type` (`type_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `resource_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `course` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_comment`
 --
 ALTER TABLE `user_comment`
-  ADD CONSTRAINT `user_comment_ibfk_1` FOREIGN KEY (`user_comment_id`) REFERENCES `comment` (`comment_id`),
-  ADD CONSTRAINT `user_comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`u_id`);
+  ADD CONSTRAINT `user_comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_comment_ibfk_1` FOREIGN KEY (`user_comment_id`) REFERENCES `comment` (`comment_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_discussion`
 --
 ALTER TABLE `user_discussion`
-  ADD CONSTRAINT `user_discussion_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`),
-  ADD CONSTRAINT `user_discussion_ibfk_2` FOREIGN KEY (`user_discussion_id`) REFERENCES `discussion` (`discussion_id`);
+  ADD CONSTRAINT `user_discussion_ibfk_2` FOREIGN KEY (`user_discussion_id`) REFERENCES `discussion` (`discussion_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_discussion_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
