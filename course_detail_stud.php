a5e12729c831c889e61be5897307edb854014867
KUNAL@KunalShah <kunshin93@yahoo.com>
2014-02-15 17:24:41 +0530

    updated c_stud

--- course_detail_stud.php
+++ course_detail_stud.php
@@ -1,3 +1,105 @@
+<?php require_once('Connections/conn.php'); ?>
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+$MM_authorizedUsers = "student";
+$MM_donotCheckaccess = "false";
+
+// *** Restrict Access To Page: Grant or deny access to this page
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+  // For security, start by assuming the visitor is NOT authorized. 
+  $isValid = False; 
+
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+  if (!empty($UserName)) { 
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
+    // Parse the strings into arrays. 
+    $arrUsers = Explode(",", $strUsers); 
+    $arrGroups = Explode(",", $strGroups); 
+    if (in_array($UserName, $arrUsers)) { 
+      $isValid = true; 
+    } 
+    // Or, you may restrict access to only certain users based on their username. 
+    if (in_array($UserGroup, $arrGroups)) { 
+      $isValid = true; 
+    } 
+    if (($strUsers == "") && false) { 
+      $isValid = true; 
+    } 
+  } 
+  return $isValid; 
+}
+
+$MM_restrictGoTo = "/dreamweaver/login.php";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+  $MM_qsChar = "?";
+  $MM_referrer = $_SERVER['PHP_SELF'];
+  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: ". $MM_restrictGoTo); 
+  exit;
+}
+?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+	  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+	  {
+		if (PHP_VERSION < 6) {
+		  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+		}
+	  
+		$theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+	  
+		switch ($theType) {
+		  case "text":
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			  break;    
+		  case "long":
+		  case "int":
+			$theValue = ($theValue != "") ? intval($theValue) : "NULL";
+			break;
+		  case "double":
+			$theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+			break;
+		  case "date":
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		  case "defined":
+			$theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+			break;
+		}
+		return $theValue;
+	  }
+	  }
+	  ?>
+<?php
+
+$colname_course_details = "-1";
+if (isset($_GET['c_id'])) {
+  $colname_course_details = $_GET['c_id'];
+}
+mysql_select_db($database_conn, $conn);
+$query_course_details = sprintf("SELECT * FROM course WHERE c_id = %s", GetSQLValueString($colname_course_details, "int"));
+$course_details = mysql_query($query_course_details, $conn) or die(mysql_error());
+$row_course_details = mysql_fetch_assoc($course_details);
+$totalRows_course_details = mysql_num_rows($course_details);
+
+mysql_select_db($database_conn, $conn);
+$query_resource = sprintf("SELECT r_id,c_id,uploaded_date,file_size,r_type,type_id,file_type,filename,download_status FROM `resource`NATURAL JOIN `resource_type` WHERE c_id=%s ",GetSQLValueString($_GET['c_id'], "int"));
+$resource = mysql_query($query_resource, $conn) or die(mysql_error());
+$row_resource = mysql_fetch_assoc($resource);
+$totalRows_resource = mysql_num_rows($resource);
+
+
+
+
+
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -35,7 +137,7 @@
 		<div id="site_title">
 			<a href="" title=""  class="site_link" target="_blank"></a>
 			<h1><a href=""rel=""></a></h1>			
-		</div>
+	  </div>
 		
 		<div id="templatemo_content">
 			<ul class="kwicks">
