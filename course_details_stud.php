51d385e5fd0c4f30f295814b5be3bcd9eab48c96
KUNAL@KunalShah <kunshin93@yahoo.com>
2014-02-19 00:19:41 +0530

    updated login links

--- course_details_stud.php
+++ course_details_stud.php
@@ -60,7 +60,7 @@ function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) {
   return $isValid; 
 }
 
-$MM_restrictGoTo = "/dreamweaver/login.php";
+$MM_restrictGoTo = "/dreamweaver/index.php#login";
 if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
   $MM_qsChar = "?";
   $MM_referrer = $_SERVER['PHP_SELF'];
