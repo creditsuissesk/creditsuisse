0087a1212080318806c93ebd846c44e94daf70f4
KUNAL@KunalShah <kunshin93@yahoo.com>
2014-02-23 15:06:14 +0530

    personalized condition for uploading resource or profile image

--- reg_data.php
+++ reg_data.php
@@ -1,40 +1,7 @@
 <?php require_once('Connections/conn.php'); ?>
 <?php /*php script for uploading pic */?>
 <?php 
-$allowedExts = array("gif", "jpeg", "jpg", "png");
-$temp = explode(".", $_FILES["file"]["name"]);
-$extension = end($temp);
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 50000)
-/*&& in_array($extension, $allowedExts)*/
-)
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-     echo '<script type="text/javascript">alert("File Error: '. $_FILES["file"]["error"] . ' ");window.location="http://localhost/dreamweaver/registration.php";</script>';
-    }
-  else
-    {
-   /* echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-	*/
-	$filename=$_POST['u_name'];
-	$upload_add="images/profiles/" . $filename;
-    if (file_exists("images/profiles/" . $filename))
-      {
-      echo  '<script type="text/javascript">alert("'. $filename . '  already exists. "); window.location="http://localhost/dreamweaver/login.php";</script>';
-      }
-    else
-      {
-      
-	  if (!function_exists("GetSQLValueString")) {
+if (!function_exists("GetSQLValueString")) {
 	  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 	  {
 		if (PHP_VERSION < 6) {
@@ -63,7 +30,52 @@ if ((($_FILES["file"]["type"] == "image/gif")
 		}
 		return $theValue;
 	  }
-	  }	  
+	  }
+
+?>
+<?php 
+if($_FILES["file"]["size"]==0 )
+{
+		echo '<script type="text/javascript">alert("Upload profile image to register"); window.location="registration.php"; </script>';
+
+}
+else
+{
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 50000)
+/*&& in_array($extension, $allowedExts)*/
+)
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+     echo '<script type="text/javascript">alert("File Error: '. $_FILES["file"]["error"] . ' ");window.location="http://localhost/dreamweaver/registration.php";</script>';
+    }
+  else
+    {
+   /* echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+	*/
+	$max_size=50000;
+	$max_size_mb=(50000/1024)/1024;
+	$filesize=$_FILES["file"]["size"]/1024/1024;
+	$filename=$_POST['u_name'];
+	$upload_add="images/profiles/" . $filename;
+    if (file_exists("images/profiles/" . $filename))
+      {
+      echo  '<script type="text/javascript">alert("Username already exists."); window.location="index.php";</script>';
+      }
+    else
+      {
 if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form")) {
 		$insertSQL = sprintf("INSERT INTO `user` (u_name, password, f_name, l_name, contact_no, dob, institute, stream,role,photo,degree,about) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s)",
 							 GetSQLValueString($_POST['u_name'], "text"),
@@ -83,34 +95,29 @@ if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form")) {
 		$Result1 = mysql_query($insertSQL, $conn) or die(mysql_error());
 		 move_uploaded_file($_FILES["file"]["tmp_name"],
       $upload_add);
-	   	echo '<script type="text/javascript">alert("File Succesfully Uploaded"); window.location="http://localhost/dreamweaver/login.php"; </script>';
+	   	echo '<script type="text/javascript">alert("Registered Successfully. Waiting for the approval by Administrator"); window.location="index.php"; </script>';
 	  }
 	  }
     }
 	    }
 else
   {
-  echo '<script type="text/javascript">alert("Invalid File. Please Register again");window.location="http://localhost/dreamweaver/registration.php";</script>';
-
+	  if (!(($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png")))
+  echo '<script type="text/javascript">alert("Invalid File Type. Please upload valid file.");  window.location="registration.php";</script>';
+  else 
+	   if(($_FILES["file"]["size"] > $max_size))
+  echo '<script type="text/javascript">alert("File Size is '.$filesize.' mb which GREATER than the allowed size. Allowed Size is '.$max_size_mb.' mb.");
+  window.location="registration.php";</script>';
+  else
+  echo '<script type="text/javascript">alert("Problem in uploading file.Please register again");window.location="registration.php";</script>';
   }
-
+}
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
