b0d6d19da8ea979d9c7c47a9b5c1cbd8e4b723e5
Shaarad Dalvi <dalvishaarad@gmail.com>
2014-03-13 19:31:37 +0530

    new conn.php

--- Connections/conn.php
+++ Connections/conn.php
@@ -3,7 +3,7 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_conn = "localhost";
-$database_conn = "credit_suisse";
+$database_conn = "cs_test";
 $username_conn = "root";
 $password_conn = "";
 $conn = mysql_pconnect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
